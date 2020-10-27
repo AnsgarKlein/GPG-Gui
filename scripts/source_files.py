a595aaa919613511cec5be18660488af496fa857
Ansgar Klein <AnsgarKlein@users.noreply.github.com>
2020-10-27 18:25:49 +0200

    Add helper script
    Add helper script that outputs all source files. This
    is useful for refreshing meson.build file.

---
+++ scripts/source_files.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+"""
+This script prints all vala source files for this project
+as relative paths starting at project root.
+"""
+
+import os, sys, re
+
+# Relative path from this script to project directory
+PRJ_PATH = [ '..' ]
+
+# Relative path from this script to source directory
+SRC_PATH = [ '..', 'src' ]
+
+def main():
+    # cd to src directory
+    os.chdir(os.path.dirname(sys.argv[0]))
+    for d in SRC_PATH:
+        os.chdir(d)
+
+    # Get all vala files
+    files = vala_files(os.path.abspath(os.getcwd()))
+
+    # Convert to relative paths
+    files = [ os.path.relpath(f, os.path.join(*PRJ_PATH)) for f in files ]
+
+    # Sort
+    files.sort()
+
+    # Print
+    for f in files:
+        print(f)
+
+def vala_files(path):
+    """Return all files recursively starting at given path"""
+
+    children = [ os.path.join(path, child) for child in os.listdir(path) ]
+    subfiles = ( child for child in children if os.path.isfile(child) )
+    subdirs  = ( child for child in children if os.path.isdir(child) )
+
+    # Create list of vala files
+    lst = []
+
+    # Add every file that ends with .vala to list
+    reg = re.compile('\.vala$', re.IGNORECASE)
+    for f in subfiles:
+        if not re.search(reg, f):
+            continue
+        lst.append(f)
+
+    # Recursively call add all vala files of subdirectories to list
+    for d in subdirs:
+        lst.extend(vala_files(d))
+
+    return lst
+
+if __name__ == '__main__':
+    main()
