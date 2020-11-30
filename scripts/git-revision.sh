8fd00e034870a38212017fbe6eee0d169ebc7208
Ansgar Klein <AnsgarKlein@users.noreply.github.com>
2020-11-30 19:32:45 +0200

    Add version script
    Add bash script that creates a version string using git describe

---
+++ scripts/git-revision.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# cd to directory of script
+if ! cd "$(dirname "$0")"; then
+    echo 'Could not cd to script directory' 2> /dev/stderr
+    exit 1
+fi
+
+git describe --tags --long --dirty --broken
