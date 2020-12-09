56f78ef87a0bd934271e0fee49af29051863a565
Ansgar Klein <AnsgarKlein@users.noreply.github.com>
2020-12-09 18:45:59 +0200

    Use version string without leading 'v'
    Try to stay posix compliant: Use sed command instead of relying
    on bash features.

--- scripts/git-revision.sh
+++ scripts/git-revision.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+
+###
+### This script is a helper script for the Meson build system.
+### It is automatically run in the build process in order to determine
+### the version string for the current build.
+###
+
 
 # cd to directory of script
 if ! cd "$(dirname "$0")"; then
@@ -6,4 +13,15 @@ if ! cd "$(dirname "$0")"; then
     exit 1
 fi
 
-git describe --tags --long --match 'v[0-9]*' --dirty --broken
+# Get version from git tags
+VERSION="$(git describe --tags --long --match 'v[0-9]*' --dirty --broken)"
+
+# Remove 'v' prefix from version string
+VERSION="$(echo "$VERSION" | sed 's/^v//')"
+
+# Error if version string is empty
+if [ -z "$VERSION" ]; then
+    exit 1
+fi
+
+echo "$VERSION"
