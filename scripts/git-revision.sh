cd9cc1591bd32c1eb0f90d1afb6deec652467195
Ansgar Klein <AnsgarKlein@users.noreply.github.com>
2021-01-15 18:56:01 +0100

    Make version script use git status before describe
    Make git revision script call git status once before calling git describe.
    Otherwise repo is sometimes labeled as '-dirty'.

--- scripts/git-revision.sh
+++ scripts/git-revision.sh
@@ -9,9 +9,12 @@
 
 # cd to directory of script
 if ! cd "$(dirname "$0")"; then
-    echo 'Could not cd to script directory' 2> /dev/stderr
+    echo 'Could not cd to script directory' > /dev/stderr
     exit 1
 fi
+
+# Run git status
+git status > /dev/null 2>&1
 
 # Get version from git tags
 VERSION="$(git describe --tags --long --match 'v[0-9]*' --dirty --broken)"
@@ -21,6 +24,7 @@ VERSION="$(echo "$VERSION" | sed 's/^v//')"
 
 # Error if version string is empty
 if [ -z "$VERSION" ]; then
+    echo 'Could not determine valid version string' > /dev/stderr
     exit 1
 fi
 
